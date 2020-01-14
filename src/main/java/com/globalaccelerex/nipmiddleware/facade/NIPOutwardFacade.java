10cdc516f6d12c394a91467edd7f5e42fa4563ea
ayodeji.ilori <ayodeji.ilori@globalaccelerex.com>
2020-01-14 11:00:09 +0100

    Implementation of Name Enquiry For NIP Outward Requests

--- src/main/java/com/globalaccelerex/nipmiddleware/facade/NIPOutwardFacade.java
+++ src/main/java/com/globalaccelerex/nipmiddleware/facade/NIPOutwardFacade.java
@@ -2,11 +2,18 @@ package com.globalaccelerex.nipmiddleware.facade;
 
 import com.globalaccelerex.nipmiddleware.exception.NIPMiddleWareAPIException;
 import com.globalaccelerex.nipmiddleware.mapper.NIPOutwardMapper;
+import com.globalaccelerex.nipmiddleware.payload.client.outward.fundstransfer.FTSingleCreditRequest;
 import com.globalaccelerex.nipmiddleware.payload.client.outward.nameenquiry.NESingleRequest;
 import com.globalaccelerex.nipmiddleware.payload.client.outward.nameenquiry.NESingleResponse;
+import com.globalaccelerex.nipmiddleware.payload.nip.outward.fundtransfer.FTSingleCreditRequestVO;
+import com.globalaccelerex.nipmiddleware.payload.nip.outward.fundtransfer.FTSingleCreditResponseVO;
 import com.globalaccelerex.nipmiddleware.payload.nip.outward.nameenquiry.NESingleRequestVO;
 import com.globalaccelerex.nipmiddleware.payload.nip.outward.nameenquiry.NESingleResponseVO;
+import com.globalaccelerex.nipmiddleware.payload.nip.outward.tsq.TsqSingleItemRequestVO;
+import com.globalaccelerex.nipmiddleware.payload.nip.outward.tsq.TsqSingleItemResponseVO;
+import com.globalaccelerex.nipmiddleware.payload.nip.ws.FundtransfersingleitemDc;
 import com.globalaccelerex.nipmiddleware.payload.nip.ws.Nameenquirysingleitem;
+import com.globalaccelerex.nipmiddleware.payload.nip.ws.Txnstatusquerysingleitem;
 import com.globalaccelerex.nipmiddleware.service.db.FundsTransferDbService;
 import com.globalaccelerex.nipmiddleware.service.ws.NIPOutwardWS;
 import com.globalaccelerex.nipmiddleware.util.SSMUtil;
@@ -15,7 +22,12 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import static com.globalaccelerex.nipmiddleware.enums.NIPResponseCodeEnum.*;
+import static com.globalaccelerex.nipmiddleware.enums.PaymentStatusEnum.FAILED;
+import static com.globalaccelerex.nipmiddleware.enums.PaymentStatusEnum.PENDING;
 
 @Slf4j
 @Service
@@ -76,6 +88,110 @@ public class NIPOutwardFacade {
         return neSingleResponse;
     }
 
+    @Async
+    public void doFundsTransferAsync(FTSingleCreditRequest ftSingleCreditRequest, String sessionId){
+        log.info("::::: Handling Async Method for Funds Transfer ::::::: ");
+        val iMarker = ftSingleCreditRequest.getMarker();
+        // do a mapping to entity and save record in db
+        final val neSingleRequest = nipOutwardMapper.mapNESingleRequest.apply(ftSingleCreditRequest);
+        neSingleRequest.setMarker(iMarker);
+        final val fundsTransferEntity = nipOutwardMapper.mapFundsTransferEntity.apply(ftSingleCreditRequest);
+        fundsTransferEntity.setSessionId(sessionId);
+        NESingleResponse neSingleResponse = null;
+        if(StringUtils.isEmpty(ftSingleCreditRequest.getNameEnquiryReference())){
+            //we need to do a nameEnquiry
+            neSingleResponse = doNameEnquiry(neSingleRequest);
+            fundsTransferEntity.setNameEnquiryReference(neSingleResponse.getSessionId());
+            if(StringUtils.isNotEmpty(ftSingleCreditRequest.getBeneficiaryBVN()) && !StringUtils.equalsIgnoreCase(ftSingleCreditRequest.getBeneficiaryBVN(),neSingleResponse.getBankVerificationNo())){
+                //the supplied BVN and the NIBSS BVN are not the same
+                //update the db
+                fundsTransferEntity.setResponseCode(NIP_104.getCode());
+                fundsTransferEntity.setPaymentStatusEnum(FAILED);
+                fundsTransferDbService.saveFundsTransferEntity(fundsTransferEntity);
+                //send a response to the client callback
+                return;
+            }
+            if(StringUtils.isEmpty(neSingleResponse.getAccountName()) || (!neSingleResponse.isSuccessResponse())){
+                //discontinue FT since we don't have a response as regards the beneficiary account name or
+                //update the db
+                fundsTransferEntity.setResponseCode(NIP_105.getCode());
+                fundsTransferEntity.setPaymentStatusEnum(FAILED);
+                fundsTransferDbService.saveFundsTransferEntity(fundsTransferEntity);
+                //send a response to the client callback
+                return;
+            }
+        }
+
+        try{
+            // go ahead with the FT
+            final val ftSingleCreditRequestVO = nipOutwardMapper.mapFTSingleCreditRequestVO.apply(ftSingleCreditRequest);
+            ftSingleCreditRequestVO.setSessionId(sessionId);
+            ftSingleCreditRequestVO.setNameEnquiryRef(neSingleResponse == null ? ftSingleCreditRequest.getNameEnquiryReference() :  neSingleResponse.getSessionId());
+
+//            sessionId = ftSingleCreditRequestVO.getSessionId();
+//            fundsTransferEntity.setSessionId(sessionId);
+            fundsTransferEntity.setPaymentStatusEnum(PENDING);
+            fundsTransferEntity.setResponseCode(NIP_09.getCode());
+            fundsTransferDbService.saveFundsTransferEntity(fundsTransferEntity);
+
+            iMarker.setRequest(" Mapping to FTSingleCreditRequestVO ", ftSingleCreditRequestVO.toString());
+            String ftSingleCreditRequestXmlString = xmlUtil.marshal(FTSingleCreditRequestVO.class, ftSingleCreditRequestVO);
+            log.info(" Raw Xml String :::: {} \n" , ftSingleCreditRequestXmlString);
+            final val encryptedXmlString = encryptString(ftSingleCreditRequestXmlString);
+            log.info(" Encrypted Xml String :::: {} \n" , encryptedXmlString);
+
+            final val fundtransfersingleitemDc = new FundtransfersingleitemDc();
+            fundtransfersingleitemDc.setRequest(encryptedXmlString);
+            iMarker.info(" Sending Request to NIPOutwardWS ");
+            final val fundTransferSingleItemDcResponse = nipOutwardWS.fundsTransfer(iMarker, fundtransfersingleitemDc);
+            iMarker.info(" Received  Response from NIPOutwardWS ");
+            if(StringUtils.isEmpty(fundTransferSingleItemDcResponse.getReturn())){
+
+                //update db
+                fundsTransferDbService.updateFTResponseCode(sessionId, NIP_106.getCode());
+                //call client endpoint
+                return;
+            }
+            log.info("Encrypted Response String from NIBSS :::::: {}" , fundTransferSingleItemDcResponse.getReturn());
+            final val ftSingleItemDcResponseXmlString = decryptString(fundTransferSingleItemDcResponse.getReturn());
+            iMarker.info(" Mapping  Response String from NIPOutwardWS to FTSingleResponseVO " + ftSingleItemDcResponseXmlString);
+
+            final val ftSingleCreditResponseVO = xmlUtil.unmarshal(ftSingleItemDcResponseXmlString, FTSingleCreditResponseVO.class);
+            iMarker.info(" FTSingleCreditResponseVO " + ftSingleCreditResponseVO.toString());
+
+            //simulate a waiting period of 60secs
+            /* */
+            log.info("----------- Stimulating 1 minute wait ---------------");
+            Thread.sleep(60000);
+            log.info("----------- Completed 1 minute wait ---------------");
+
+            final val tsqSingleItemRequestVO = nipOutwardMapper.buildTsqSingleItemRequestVO(ftSingleCreditResponseVO.getSessionId());
+
+            String tsqSingleItemRequestXmlString = xmlUtil.marshal(TsqSingleItemRequestVO.class, tsqSingleItemRequestVO);
+            log.info(" Raw Xml String :::: {} \n" , tsqSingleItemRequestXmlString);
+            final val encryptedTsqSingleItemRequestXmlString = encryptString(tsqSingleItemRequestXmlString);
+            log.info(" Encrypted Xml String :::: {} \n" , encryptedTsqSingleItemRequestXmlString);
+
+            final val txnstatusquerysingleitem = new Txnstatusquerysingleitem();
+            txnstatusquerysingleitem.setRequest(encryptedTsqSingleItemRequestXmlString);
+
+            final val txnStatusQuerySingleItemResponse = nipOutwardWS.txnStatus(iMarker, txnstatusquerysingleitem);
+
+            final val tsqSingleItemResponseXmlString = decryptString(txnStatusQuerySingleItemResponse.getReturn());
+            log.info(" Clear String response :: {}" , tsqSingleItemResponseXmlString);
+
+            final val tsqSingleItemResponseVO = xmlUtil.unmarshal(tsqSingleItemResponseXmlString, TsqSingleItemResponseVO.class);
+            iMarker.info(" TsqSingleItemResponseVO " + tsqSingleItemResponseVO.toString());
+
+            //update db
+            fundsTransferDbService.updateFTResponseCode(tsqSingleItemResponseVO.getSessionId(), tsqSingleItemResponseVO.getResponseCode());
+            //call client endpoint and push response to him
+        }catch(Exception ex){
+            log.error("Caught this exception ::::: {}", ex);
+            fundsTransferDbService.updateFTResponseCode(sessionId, NIP_107.getCode());
+
+        }
+    }
     private String encryptString(String clearString){
         return ssmUtil.encryptRequest(clearString);
     }
--- src/main/java/com/globalaccelerex/nipmiddleware/service/ws/NIPOutwardWS.java
+++ src/main/java/com/globalaccelerex/nipmiddleware/service/ws/NIPOutwardWS.java
@@ -3,8 +3,7 @@ package com.globalaccelerex.nipmiddleware.service.ws;
 import com.globalaccelerex.nipmiddleware.config.AppConfig;
 import com.globalaccelerex.nipmiddleware.exception.NIPMiddleWareAPIException;
 import com.globalaccelerex.nipmiddleware.logging.api.IMarker;
-import com.globalaccelerex.nipmiddleware.payload.nip.ws.Nameenquirysingleitem;
-import com.globalaccelerex.nipmiddleware.payload.nip.ws.NameenquirysingleitemResponse;
+import com.globalaccelerex.nipmiddleware.payload.nip.ws.*;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +12,7 @@ import org.springframework.ws.soap.SoapMessage;
 
 import javax.xml.bind.JAXBElement;
 
-import static com.globalaccelerex.nipmiddleware.api.NipAPI.NAME_ENQUIRY_OUTWARD;
+import static com.globalaccelerex.nipmiddleware.api.NipAPI.*;
 
 
 @Service
@@ -43,5 +42,45 @@ public class NIPOutwardWS extends WebServiceGatewaySupport {
         }
         marker.info("done with Name Enquiry");
         return nameenquirysingleitemResponse;
+    }
+
+    public FundtransfersingleitemDcResponse fundsTransfer(IMarker marker, FundtransfersingleitemDc fundtransfersingleitemDc) {
+
+        val webServiceURL = appConfig.getBaseUrl();
+        //val soapActionURL =appConfig.getSoapActionUrl() + "fundtransfersingleitem_dcRequest";
+        val soapActionURL =appConfig.getSoapActionUrl() + FUNDS_TRANSFER_OUTWARD;
+        FundtransfersingleitemDcResponse fundTransferSingleItemDcResponse = new FundtransfersingleitemDcResponse();
+        marker.info(" ====== Doing FundsTransfer ====== ");
+        try {
+            val response = (JAXBElement<FundtransfersingleitemDcResponse>) getWebServiceTemplate()
+                    .marshalSendAndReceive(webServiceURL, fundtransfersingleitemDc,
+                            message -> ((SoapMessage) message).setSoapAction(soapActionURL));
+            fundTransferSingleItemDcResponse = response.getValue();
+        } catch (Exception ex) {
+            marker.info(ex);
+            throw new NIPMiddleWareAPIException("Error",ex.getMessage(),false);
+        }
+        marker.info("done with FundsTransfer DC");
+        return fundTransferSingleItemDcResponse;
+    }
+
+    public TxnstatusquerysingleitemResponse txnStatus(IMarker marker, Txnstatusquerysingleitem txnstatusquerysingleitem){
+        val webServiceURL = appConfig.getTsqUrl();
+        val soapActionURL = appConfig.getSoapActionUrl() + TSQ_OUTWARD;
+
+        TxnstatusquerysingleitemResponse txnStatusQuerySingleItemResponse = new TxnstatusquerysingleitemResponse();
+        marker.info(" ====== Doing TSQ ====== ");
+        try{
+            val response = (JAXBElement<TxnstatusquerysingleitemResponse>) getWebServiceTemplate()
+                    .marshalSendAndReceive(webServiceURL, txnstatusquerysingleitem,
+                            message -> ((SoapMessage) message).setSoapAction(soapActionURL));
+            txnStatusQuerySingleItemResponse = response.getValue();
+        }catch (Exception ex) {
+            marker.info(ex);
+            throw new NIPMiddleWareAPIException("Error",ex.getMessage(),false);
+        }
+        marker.info("done with  TSQ");
+        return txnStatusQuerySingleItemResponse;
+
     }
 }
