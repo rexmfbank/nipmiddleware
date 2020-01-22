c0b5dfcfebe114f248cb034a48c23a2d04ee1aa8
ayodeji.ilori <ayodeji.ilori@globalaccelerex.com>
2020-01-22 15:15:11 +0100

    Authorization and Authentication for clients

--- src/main/java/com/globalaccelerex/nipmiddleware/config/SecurityConfig.java
+++ src/main/java/com/globalaccelerex/nipmiddleware/config/SecurityConfig.java
@@ -1,14 +1,54 @@
 package com.globalaccelerex.nipmiddleware.config;
 
+import com.globalaccelerex.nipmiddleware.security.CustomAuthenticationFilter;
+import com.globalaccelerex.nipmiddleware.security.CustomAuthenticationProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static com.globalaccelerex.nipmiddleware.api.ClientAPI.NIP_OUTWARD_API;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher(NIP_OUTWARD_API + "/**").
+                csrf().disable().
+                cors().and().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().
+                authorizeRequests().
+                anyRequest().authenticated().
+                and().
+                anonymous().disable().
+                exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
+        http.addFilterBefore(new CustomAuthenticationFilter(authenticationManager()), BasicAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticationProvider);
+    }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedEntryPoint() {
+        return (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
 }
