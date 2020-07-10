f6fd92fbed77ee18c291562821a67ee0e0c032e5
ayojava <ayojava@gmail.com>
2020-07-10 11:40:45 +0100

    Disabled shutting down of service

--- src/main/java/com/globalaccelerex/nipmiddleware/util/SystemSettingUtil.java
+++ src/main/java/com/globalaccelerex/nipmiddleware/util/SystemSettingUtil.java
@@ -2,7 +2,6 @@ package com.globalaccelerex.nipmiddleware.util;
 
 import com.globalaccelerex.nipmiddleware.service.db.SystemSettingDbService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,12 +24,15 @@ public class SystemSettingUtil {
         this.systemSettingDbService = systemSettingDbService;
     }
 
+    //@TODO remove this method going forward
     public void changeStatus(String name , String value){
-        systemSettingDbService.updateServiceStatus(name, value);
+        //systemSettingDbService.updateServiceStatus(name, value);
     }
 
+    //@TODO remove this check going forward
     public boolean isNibssStatusDown(){
-        return StringUtils.equalsIgnoreCase(systemSettingDbService.findStatus(CALL_NIBSS_API).getValue(),DOWN_STATUS);
+        //return StringUtils.equalsIgnoreCase(systemSettingDbService.findStatus(CALL_NIBSS_API).getValue(),DOWN_STATUS);
+        return false;
     }
 
 
