2178d2532a437190c997dbd0cf19fea74128e9be
Liasu Hassan <htope68@gmail.com>
2025-03-10 19:42:43 +0000

    log request

--- src/main/java/com/globalaccelerex/nipmiddleware/security/client/ClientAuthenticationFilter.java
+++ src/main/java/com/globalaccelerex/nipmiddleware/security/client/ClientAuthenticationFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.TimeZone;
 
 import static com.globalaccelerex.nipmiddleware.enums.NIPResponseCodeEnum.NIP_201;
@@ -46,12 +47,14 @@ public class ClientAuthenticationFilter extends GenericFilterBean {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        try{
+        logRequestDetails(request);
+
+        try {
             attemptAuthentication(request, response);
             filterChain.doFilter(request, response);
-        }catch (AuthenticationException authenticationException) {
+        } catch (AuthenticationException authenticationException) {
             SecurityContextHolder.clearContext();
-            log.error("Internal authentication service exception => " +authenticationException.getMessage());
+            log.error("Internal authentication service exception => " + authenticationException.getMessage());
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
             if (authenticationException instanceof AccessControlException) {
@@ -62,19 +65,18 @@ public class ClientAuthenticationFilter extends GenericFilterBean {
             } else {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authenticationException.getMessage());
             }
-        }catch (CacheLoader.InvalidCacheLoadException i ){
+        } catch (CacheLoader.InvalidCacheLoadException i) {
             SecurityContextHolder.clearContext();
-            log.error("Client not found"+ i.getMessage());
+            log.error("Client not found" + i.getMessage());
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.addHeader("Content-Type", "application/json");
             final val errorResponse = new ErrorResponse();
             errorResponse.setResponseCode(NIP_201.getCode());
             errorResponse.setResponseMessage(CLIENT_NOT_FOUND_MSG);
             response.getWriter().print(OBJECT_MAPPER.writeValueAsString(errorResponse));
-        }
-        catch (Exception ex){
+        } catch (Exception ex) {
             SecurityContextHolder.clearContext();
-            log.error("Client Authentication exception "+ ex.getMessage());
+            log.error("Client Authentication exception " + ex.getMessage());
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.addHeader("Content-Type", "application/json");
 
@@ -83,7 +85,19 @@ public class ClientAuthenticationFilter extends GenericFilterBean {
             errorResponse.setResponseMessage(UNAUTHORIZED_ACCESS_MSG);
             response.getWriter().print(OBJECT_MAPPER.writeValueAsString(errorResponse));
         }
+    }
 
+    private void logRequestDetails(HttpServletRequest request) {
+        log.info("Request Method: " + request.getMethod());
+        log.info("Request URL: " + request.getRequestURL().toString());
+        log.info("Request Headers: ");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            log.info("Header Name: {} :: Header Value: {}", headerName, headerValue);
+        }
     }
 
     private void attemptAuthentication(HttpServletRequest request,
